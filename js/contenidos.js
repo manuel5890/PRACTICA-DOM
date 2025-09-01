6d0771c05c96941861c02b16d0acfb46c21256ad
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-01 17:01:33 -0500

    agregue manipulacion de contenidos con js

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,5 +1,21 @@
+// cambiar contenidos a elementos unicos
+
 const caja = document.getElementById('caja-unica');
+const estilos_clases = document.querySelector('#estilos_clases');
+
+
 
 caja.innerText="cambio en el texto #1 con innerText";
 caja.innerHTML="cambio en el texto #2 con innerHTML";
 caja.textContent="cambio en el texto #3 con textContent";
+estilos_clases.innerText="manipulacion de estilos css y clases"
+
+// cambiar contenidos a elementos multiples
+
+const cajas_contenidos = document.querySelectorAll('.caja-contenidos');
+cajas_contenidos.forEach((caja, index) => {
+caja.innerText="caja multiple numero #" + index
+caja.style.color="blue"
+caja.style.fontSize = "45px"
+caja.style.textAlign="right"
+});
