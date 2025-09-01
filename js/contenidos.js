8b573a7d7c9864d69567a4e170a4e9b33c544b5b
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-01 15:46:31 -0500

    agregue la funcionalidad para cambiar el texto o contenido de una caja

---
+++ js/contenidos.js
@@ -0,0 +1,5 @@
+const caja = document.getElementById('caja-unica');
+
+caja.innerText="cambio en el texto #1 con innerText";
+caja.innerHTML="cambio en el texto #2 con innerHTML";
+caja.textContent="cambio en el texto #3 con textContent";
